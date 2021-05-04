9c7e6f1849dc97b2069a999d1d83d1f3bca583a3
Yuping Huang <yupinghuang@users.noreply.github.com>
2021-05-04 08:00:17 -0700

    Single thread read data.

--- src/cxx/gridding.cpp
+++ src/cxx/gridding.cpp
@@ -6,14 +6,19 @@
 
 #include <casacore/casa/Arrays/Array.h>
 #include <casacore/tables/Tables/ArrayColumn.h>
+#include <casacore/tables/Tables/ScalarColumn.h>
 #include <casacore/ms/MeasurementSets/MeasurementSet.h>
 
 #include <iostream>
 #include <iomanip>
+#include <memory>
 #include <cstdlib>  // size_t
 #include <complex>
 #include <limits>
-#include <complex>
+#include <stdexcept>
+#include <chrono>
+
+#include <omp.h>
 
 using std::complex; using std::string;
 
@@ -28,42 +33,59 @@ void read_from_ms(idg::UVW<float>& uvw, idg::Array1D<float>& freqs,
     1 + 1;
 }
 
-void grid() {
-    /*
-     * Call this from python?
-     */
-    // parameters
+
+int main (int argc, char *argv[]) {
+    string ms_path = "/fastpool/data/20210226M-2GHz-1chan-600int.ms";
+    casacore::MeasurementSet ms(ms_path);
+    casacore::ArrayColumn<casacore::Complex> data_column(
+            ms, casacore::MS::columnName(casacore::MSMainEnums::DATA));
+    casacore::ArrayColumn<double> uvw_column(
+            ms, casacore::MS::columnName(casacore::MSMainEnums::UVW));
+    casacore::ScalarColumn<int> ant1(
+            ms, casacore::MS::columnName(casacore::MSMainEnums::ANTENNA1));
+    casacore::ScalarColumn<int> ant2(
+            ms, casacore::MS::columnName(casacore::MSMainEnums::ANTENNA2));
+
     unsigned int nr_correlations = 4;
-    unsigned int nr_stations = 9;
-    unsigned int nr_channels = 9;
-    unsigned int nr_timesteps = 2048;
-    unsigned int nr_timeslots = 7;
-    unsigned int grid_size = 2048;
-    unsigned int subgrid_size = 32;
-    unsigned int kernel_size = 9;
+    unsigned int nr_rows = data_column.nrow();
+    unsigned int nr_stations = ms.antenna().nrow();
     unsigned int nr_baselines = (nr_stations * (nr_stations - 1)) / 2;
-    float integration_time = 1.0f;
+    if (nr_rows % nr_baselines != 0 )
+        throw std::length_error("Data column should be divisible by nr_correlations.");
+    size_t nr_timesteps = nr_rows / nr_baselines;
 
-    idg::Data data = idg::get_example_data(nr_baselines, grid_size,
-                                           integration_time, nr_channels);
+    std::clog << "nr_baselines = " << nr_baselines << std::endl;
+    std::clog << "nr_rows = " << nr_rows << std::endl;
+    std::clog << "nr_stations = " << nr_stations << std::endl;
+    std::clog << "nr_timesteps = " << nr_timesteps << std::endl;
 
-    float image_size = data.compute_image_size(grid_size, nr_channels);
+    float integration_time = 1.5f;
+
+    casacore::ROScalarColumn<int> numChanCol(ms.spectralWindow(), casacore::MSSpectralWindow::columnName(
+                      casacore::MSSpectralWindowEnums::NUM_CHAN));
+    int nr_channels;
+    numChanCol.get(0, nr_channels);
+    std::clog << "nr_channels = " << nr_channels << std::endl;
+
+    unsigned int nr_timeslots = 1; // timeslot for a-term
+    unsigned int subgrid_size = 32;
+    unsigned int kernel_size = 13;
+
+
+    // hard-coding for now
+    unsigned int grid_size = 8192;
+    float image_size = 0.083367;
     float cell_size = image_size / grid_size;
 
-    std::clog << ">>> Initialize data structures" << std::endl;
+    std::clog << ">>> Initialize IDG data structures" << std::endl;
     idg::proxy::cuda::Generic proxy;
 
-    // Try to allocate aligned memory
+    // Allocate memory with Proxy
     idg::Array1D<float> frequencies = proxy.allocate_array1d<float>(nr_channels);
-    data.get_frequencies(frequencies, image_size);
-
     idg::Array2D<idg::UVW<float>> uvw =
             proxy.allocate_array2d<idg::UVW<float>>(nr_baselines, nr_timesteps);
-    data.get_uvw(uvw);
-
     idg::Array3D<idg::Visibility<std::complex<float>>> visibilities =
-            idg::get_dummy_visibilities(proxy, nr_baselines, nr_timesteps,
-                                        nr_channels);
+            proxy.allocate_array3d<idg::Visibility<std::complex<float>>>(nr_baselines, nr_timesteps, nr_correlations);
 
     idg::Array1D<std::pair<unsigned int, unsigned int>> baselines =
             idg::get_example_baselines(proxy, nr_stations, nr_baselines);
@@ -76,9 +98,33 @@ void grid() {
             idg::get_example_spheroidal(proxy, subgrid_size, subgrid_size);
     idg::Array1D<float> shift = idg::get_zero_shift();
     auto grid = proxy.allocate_grid(1, nr_correlations, grid_size, grid_size);
-
     proxy.set_grid(grid);
-    std::clog << std::endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::clog << "Reading measurement set." << std::endl;
+
+    // Read and reorder data
+
+    for (unsigned int bl=0; bl < nr_baselines; ++bl) {
+            for (unsigned int t=0; t < nr_timesteps; ++t) {
+                    casacore::Array<std::complex<float>> row = data_column.get(bl + t * nr_baselines);
+                    for (unsigned int chan=0; chan < nr_channels; ++chan) {
+                            casacore::IPosition xx(2, chan, 0);
+                            casacore::IPosition xy(2, chan, 1);
+                            casacore::IPosition yx(2, chan, 2);
+                            casacore::IPosition yy(2, chan, 3);
+                            idg::Matrix2x2<std::complex<float>> vis = {row(xx), row(xy), row(yx), row(yy)};
+                            visibilities(bl, t, chan) = vis;
+                    }
+
+            }
+        
+    }
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
+    std::clog << "Done reading measurement set in " << duration.count() << "s" << std::endl;
+    return 0;
 
     // not using w_step
     proxy.init_cache(subgrid_size, cell_size, 0.0, shift);
@@ -95,14 +141,4 @@ void grid() {
     proxy.gridding(*plan, frequencies, visibilities, uvw, baselines, aterms,
                    aterms_offsets, spheroidal);
     proxy.get_final_grid();
-    // result is at grid->data()
-    // TODO: remove taper
-}
-
-int main (int argc, char *argv[]) {
-    string ms_path = "/fastpool/data/20210226M-700MHz-1chan-600int.ms";
-    auto ms = casacore::MeasurementSet(ms_path);
-    casacore::ArrayColumn<casacore::Complex> data_column(
-            ms, casacore::MS::columnName(casacore::MSMainEnums::DATA));
-    std::clog << data_column.shape(0) << std::endl;
 }

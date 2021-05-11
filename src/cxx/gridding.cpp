921be9e9ed50e14609a285433d4902f701ad3e90
Yuping Huang <yupinghyper@gmail.com>
2021-05-11 16:32:09 -0700

    yay we're gridding

--- src/cxx/gridding.cpp
+++ src/cxx/gridding.cpp
@@ -21,22 +21,46 @@
 
 #include <omp.h>
 
-using std::complex; using std::string;
+using std::complex; using std::string; using casacore::IPosition;
 
-void read_from_ms(idg::UVW<float>& uvw, idg::Array1D<float>& freqs,
-                  idg::Array1D<std::pair<unsigned int, unsigned  int>>& baselines,
-                  idg::Array3D<idg::Visibility<complex<float>>>& vis, const string& ms_path) {
-    //construct valueholder from the allocated arrays
 
-    // vis_arr_casa = casacore::Array<complex>(shape, vis_ptr, casacore::StorageInitPolicy::SHARE)
-    //
-    // oh wait I need to reorder the data.
-    1 + 1;
+void get_metadata(const string &ms_path, float &integration_time,
+                  unsigned int &nr_rows, unsigned int &nr_stations,
+                  unsigned int &nr_baselines, unsigned int &nr_timesteps,
+                  unsigned int &nr_channels, unsigned int &nr_correlations) {
+    casacore::MeasurementSet ms(ms_path);
+    casacore::ROArrayColumn<casacore::Complex> data_column(
+            ms, casacore::MS::columnName(casacore::MSMainEnums::DATA));
+    nr_correlations = 4;
+    nr_rows = data_column.nrow();
+    nr_stations = ms.antenna().nrow();
+    nr_baselines = (nr_stations * (nr_stations - 1)) / 2;
+    // assume there's no autocorrelation in the data
+    assert(nr_rows % nr_baselines == 0);
+    nr_timesteps = nr_rows / nr_baselines;
+
+    integration_time = 1.5f;
+
+    casacore::ROScalarColumn<int> numChanCol(ms.spectralWindow(), casacore::MSSpectralWindow::columnName(
+                      casacore::MSSpectralWindowEnums::NUM_CHAN));
+    int nr_channels_tmp;
+    numChanCol.get(0, nr_channels_tmp);
+    nr_channels = nr_channels_tmp;
+    std::clog << "integration_time = " << integration_time << std::endl;
+    std::clog << "nr_rows = " << nr_rows << std::endl;
+    std::clog << "nr_stations = " << nr_stations << std::endl;
+    std::clog << "nr_baselines = " << nr_baselines << std::endl;
+    std::clog << "nr_timesteps = " << nr_timesteps << std::endl;
+    std::clog << "nr_channels = " << nr_channels << std::endl;
+    std::clog << "nr_correlations = " << nr_correlations << std::endl;
 }
 
-
-int main (int argc, char *argv[]) {
-    string ms_path = "/fastpool/data/20210226M-2GHz-1chan-600int.ms";
+void get_data(const string &ms_path, const unsigned int nr_channels,
+              const unsigned int nr_baselines, const unsigned int nr_timesteps,
+              idg::Array2D<idg::UVW<float>> &uvw, idg::Array1D<float> &frequencies,
+              idg::Array1D<std::pair<unsigned int, unsigned int>> &baselines,
+              idg::Array3D<idg::Visibility<complex<float>>> &visibilities)
+{
     casacore::MeasurementSet ms(ms_path);
     casacore::ROArrayColumn<casacore::Complex> data_column(
             ms, casacore::MS::columnName(casacore::MSMainEnums::DATA));
@@ -46,75 +70,24 @@ int main (int argc, char *argv[]) {
             ms, casacore::MS::columnName(casacore::MSMainEnums::ANTENNA1));
     casacore::ROScalarColumn<int> ant2(
             ms, casacore::MS::columnName(casacore::MSMainEnums::ANTENNA2));
-
-    const unsigned int nr_correlations = 4;
-    const unsigned int nr_rows = data_column.nrow();
-    const unsigned int nr_stations = ms.antenna().nrow();
-    const unsigned int nr_baselines = (nr_stations * (nr_stations - 1)) / 2;
-    if (nr_rows % nr_baselines != 0 )
-        throw std::length_error("Data column should be divisible by nr_correlations.");
-    const unsigned int nr_timesteps = nr_rows / nr_baselines;
-
-    std::clog << "nr_baselines = " << nr_baselines << std::endl;
-    std::clog << "nr_rows = " << nr_rows << std::endl;
-    std::clog << "nr_stations = " << nr_stations << std::endl;
-    std::clog << "nr_timesteps = " << nr_timesteps << std::endl;
-
-    const float integration_time = 1.5f;
-
-    casacore::ROScalarColumn<int> numChanCol(ms.spectralWindow(), casacore::MSSpectralWindow::columnName(
-                      casacore::MSSpectralWindowEnums::NUM_CHAN));
     casacore::ROArrayColumn<double> freqs(ms.spectralWindow(), casacore::MSSpectralWindow::columnName(
                     casacore::MSSpectralWindowEnums::CHAN_FREQ));
-    int nr_channels;
-    numChanCol.get(0, nr_channels);
-    std::clog << "nr_channels = " << nr_channels << std::endl;
-    std::clog << "freqs.nrow() = " << freqs.nrow() << std::endl;
-    std::clog << "freqs row 0 shape = " << freqs.shape(0) << std::endl;
-    
-    const unsigned int nr_timeslots = 1; // timeslot for a-term
-    const unsigned int subgrid_size = 32;
-    const unsigned int kernel_size = 13;
 
-
-    // hard-coding for now
-    unsigned int grid_size = 8192;
-    float image_size = 0.083367;
-    float cell_size = image_size / grid_size;
-    std::clog << "grid_size = " << grid_size << std::endl;
-    std::clog << "image_size = " << image_size << std::endl;
-    std::clog << "cell_size = " << cell_size << std::endl;
-
-    std::clog << ">>> Initialize IDG data structures" << std::endl;
-    idg::proxy::cuda::Generic proxy;
-
-    // Allocate memory with Proxy
-    idg::Array1D<float> frequencies = proxy.allocate_array1d<float>(nr_channels);
-    idg::Array2D<idg::UVW<float>> uvw =
-            proxy.allocate_array2d<idg::UVW<float>>(nr_baselines, nr_timesteps);
-    idg::Array3D<idg::Visibility<std::complex<float>>> visibilities =
-            proxy.allocate_array3d<idg::Visibility<std::complex<float>>>(nr_baselines, nr_timesteps, nr_correlations);
-
-    idg::Array1D<std::pair<unsigned int, unsigned int>> baselines =
-            idg::get_example_baselines(proxy, nr_stations, nr_baselines);
-    idg::Array4D<idg::Matrix2x2<std::complex<float>>> aterms =
-            idg::get_example_aterms(proxy, nr_timeslots, nr_stations, subgrid_size,
-                                    subgrid_size);
-    idg::Array1D<unsigned int> aterms_offsets =
-            idg::get_example_aterms_offsets(proxy, nr_timeslots, nr_timesteps);
-    idg::Array2D<float> spheroidal =
-            idg::get_example_spheroidal(proxy, subgrid_size, subgrid_size);
-    idg::Array1D<float> shift = idg::get_zero_shift();
-    auto grid = proxy.allocate_grid(1, nr_correlations, grid_size, grid_size);
-    proxy.set_grid(grid);
-
-    std::clog << "Reading baseline and frequencies data from measurement set." << std::endl;
+    std::clog << "Reading baseline pairs and frequencies data from the measurement set." << std::endl;
     casacore::Array<double> src_freqs = freqs(0);
     assert(src_freqs.size() == nr_channels);
     for (unsigned int i = 0; i < nr_channels; ++i)
-        frequencies(i) = float(src_freqs(casacore::IPosition(1, i)));
-    std::clog << uvw_column.nrow() << std::endl;
-    std::clog << uvw_column.shape(0) << std::endl;
+        frequencies(i) = float(src_freqs(IPosition(1, i)));
+    std::clog << "done with reading frequencies." << std::endl;
+
+    casacore::Slicer first_int_rows(IPosition(1, 0), IPosition(1, nr_baselines)); 
+    casacore::Vector<int> ant1_vec = ant1.getColumnRange(first_int_rows); 
+    casacore::Vector<int> ant2_vec = ant2.getColumnRange(first_int_rows); 
+    #pragma omp parallel for default(none) shared(baselines, ant1_vec, ant2_vec)
+    for (unsigned int i = 0; i < nr_baselines; ++i) {
+        std::pair<unsigned int, unsigned int> curr_pair = {ant1_vec(i), ant2_vec(i)};
+        baselines(i) = curr_pair;
+    }
 
     std::chrono::_V2::system_clock::time_point start = std::chrono::high_resolution_clock::now();
     std::chrono::_V2::system_clock::time_point stop;
@@ -129,25 +102,31 @@ int main (int argc, char *argv[]) {
      * casacore doesn't seem to like it within omp parallel.
     **/
 
-    // casacore::Array<std::complex<float>> rows(casacore::IPosition(3, 4, nr_channels, data_column.nrow()));
-    casacore::Array<std::complex<float>> rows = data_column.getColumn();
+    const casacore::Array<complex<float>> data_rows = data_column.getColumn();
+    const casacore::Array<double> uvw_rows = uvw_column.getColumn();
     stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-    start = std::chrono::high_resolution_clock::now();
     std::clog << "Done reading measurement set in " << duration.count() << "s" << std::endl;
+    start = std::chrono::high_resolution_clock::now();
 
-    // TODO use rows::const_iterator?
-    #pragma omp parallel for default(none) shared(visibilities, nr_channels, rows)
-    for (unsigned int bl=0; bl < nr_baselines; ++bl) {
-        for (unsigned int t=0; t < nr_timesteps; ++t) {
-            // transpose the other things?
+    #pragma omp parallel for default(none) shared(visibilities, uvw_column, uvw)
+    for (unsigned int t=0; t < nr_timesteps; ++t) {
+        for (unsigned int bl=0; bl < nr_baselines; ++bl) {
+            unsigned int row_i = bl + t * nr_baselines;
+
+            idg::UVW<float> idg_uvw = {
+                float(uvw_rows(IPosition(2, 0, row_i))),
+                float(uvw_rows(IPosition(2, 1, row_i))),
+                float(uvw_rows(IPosition(2, 2, row_i)))};
+            uvw(bl, t) = idg_uvw;
+            
             for (unsigned int chan=0; chan < nr_channels; ++chan) {
                 unsigned row_nr = bl + t * nr_baselines;
-                idg::Matrix2x2<std::complex<float>> vis = {
-                        rows(casacore::IPosition(3, 0, chan, row_nr)),
-                        rows(casacore::IPosition(3, 1, chan, row_nr)),
-                        rows(casacore::IPosition(3, 2, chan, row_nr)),
-                        rows(casacore::IPosition(3, 3, chan, row_nr))};
+                idg::Matrix2x2<complex<float>> vis = {
+                        data_rows(IPosition(3, 0, chan, row_i)),
+                        data_rows(IPosition(3, 1, chan, row_i)),
+                        data_rows(IPosition(3, 2, chan, row_i)),
+                        data_rows(IPosition(3, 3, chan, row_i))};
                 visibilities(bl, t, chan) = vis;
             }
         }
@@ -155,12 +134,65 @@ int main (int argc, char *argv[]) {
     stop = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     std::clog << "Reordered visibilities in " << duration.count() << "s" << std::endl;
-    return 0;
-    // not using w_step
+}
+
+int main (int argc, char *argv[]) {
+    string ms_path = "/fastpool/data/20210226M-2GHz-1chan-600int.ms";
+
+    // TODO: a struct?
+    unsigned int nr_correlations;
+    unsigned int nr_rows;
+    unsigned int nr_stations;
+    unsigned int nr_baselines;
+    float integration_time;
+    unsigned int nr_timesteps;
+    unsigned int nr_channels;
+    get_metadata(ms_path, integration_time, nr_rows, nr_stations,
+                 nr_baselines, nr_timesteps, nr_channels, nr_correlations);
+    
+
+    // hard-coding for now
+    unsigned int grid_size = 8192;
+    float image_size = 0.083367;
+    float cell_size = image_size / grid_size;
+    std::clog << "grid_size = " << grid_size << std::endl;
+    std::clog << "image_size = " << image_size << std::endl;
+    std::clog << "cell_size = " << cell_size << std::endl;
+
+    std::clog << ">>> Initialize IDG proxy." << std::endl;
+    idg::proxy::cuda::Generic proxy;
+
+    idg::Array1D<float> frequencies = proxy.allocate_array1d<float>(nr_channels);
+    idg::Array1D<std::pair<unsigned int, unsigned int>> baselines =
+        proxy.allocate_array1d <std::pair<unsigned int, unsigned int>>(nr_baselines);
+    idg::Array2D<idg::UVW<float>> uvw =
+        proxy.allocate_array2d<idg::UVW<float>>(nr_baselines, nr_timesteps);
+    idg::Array3D<idg::Visibility<complex<float>>> visibilities =
+            proxy.allocate_array3d<idg::Visibility<complex<float>>>(nr_baselines, nr_timesteps, nr_channels);
+
+    get_data(ms_path, nr_channels, nr_baselines, nr_timesteps, uvw, frequencies, baselines, visibilities);
+
+    // A-terms
+    const unsigned int nr_timeslots = 1; // timeslot for a-term
+    const unsigned int subgrid_size = 32;
+    const unsigned int kernel_size = 13;
+    idg::Array4D<idg::Matrix2x2<complex<float>>> aterms =
+        idg::get_example_aterms(proxy, nr_timeslots, nr_stations, subgrid_size,
+                                subgrid_size);
+    idg::Array1D<unsigned int> aterms_offsets =
+            idg::get_example_aterms_offsets(proxy, nr_timeslots, nr_timesteps);
+
+    idg::Array2D<float> spread =
+            idg::get_example_spheroidal(proxy, subgrid_size, subgrid_size);
+    idg::Array1D<float> shift = idg::get_zero_shift();
+
+    std::shared_ptr<idg::Grid> grid = proxy.allocate_grid(1, nr_correlations, grid_size, grid_size);
+    proxy.set_grid(grid);
+    // no w-tiling, i.e. not using w_step
     proxy.init_cache(subgrid_size, cell_size, 0.0, shift);
 
     // Create plan
-    std::clog << ">>> Create plan" << std::endl;
+    std::clog << ">>> Creating plan" << std::endl;
     idg::Plan::Options options;
     options.plan_strict = true;
     const std::unique_ptr<idg::Plan> plan = proxy.make_plan(
@@ -169,6 +201,6 @@ int main (int argc, char *argv[]) {
 
     std::clog << ">>> Run gridding" << std::endl;
     proxy.gridding(*plan, frequencies, visibilities, uvw, baselines, aterms,
-                   aterms_offsets, spheroidal);
+                   aterms_offsets, spread);
     proxy.get_final_grid();
 }
